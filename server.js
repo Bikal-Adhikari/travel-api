1388199877712189346c40922b778a797c321333
Bikal <bikaladhikari5@gmail.com>
2024-04-29 19:34:04 +1000

    Server Initialized and database connected

---
+++ server.js
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 7000;
+
+// MongoDB connect
+import { connectDB } from "./src/config/dbConfig.js";
+connectDB();
+
+import morgan from "morgan";
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+
+// middlewares
+import cors from "cors";
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log(`server running at http://localhost:${PORT}`);
+});
