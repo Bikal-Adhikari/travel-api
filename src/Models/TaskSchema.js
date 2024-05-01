1d564c8560198ce9b2ec2fcd85f38729f67f1ea9
Bikal <bikaladhikari5@gmail.com>
2024-05-01 17:49:42 +1000

    TaskSchema to connect to database is formed

---
+++ src/Models/TaskSchema.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+  name: { type: "string", required: true },
+  info: { type: "string", required: true },
+  image: { type: "string", required: true },
+  price: { type: "string", required: true },
+  duration: { type: "string", required: true },
+  highlights: { type: ["string"], required: true },
+  includes: { type: ["string"], required: true },
+});
+
+export default mongoose.models("Task", taskSchema);
